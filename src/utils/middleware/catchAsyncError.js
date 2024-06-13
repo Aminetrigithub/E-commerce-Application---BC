3c76fd021d4d74f0197824b892c4326aad3efad4
Amine Trigui <aminetrigui.github@gmail.com>
2024-06-13 20:55:44 +0100

    errorHandling

--- src/utils/middleware/catchAsyncError.js
+++ src/utils/middleware/catchAsyncError.js
@@ -3,9 +3,9 @@ import AppError from "../services/AppError.js";
 const catchAsyncError = (fn) => {
   return (req, res, next) => {
     fn(req, res, next)
-    // .catch((err) => {
-    // next(new AppError("Something went wrong", 406));
-    // });
+    .catch((err) => {
+    next(new AppError("Something went wrong", 406));
+    });
   };
 };
 
