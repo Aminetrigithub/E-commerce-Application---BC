9eee73e455672c7f58968ea4736035eeebf28f9c
Amine Trigui <aminetrigui.github@gmail.com>
2024-07-22 22:37:13 +0100

    refactoring filesUpload, upload array fo files, createProduct

--- src/modules/products/product.routes.js
+++ src/modules/products/product.routes.js
@@ -1,29 +1,30 @@
 import express from 'express'
 import * as productController from './product.controller.js'
 import multer from 'multer';
+import { uploadMixFiles } from '../../utils/middleware/fileUploads.js';
 
 
 const productRouter = express.Router()
 
 // multer product
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/product");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + "-" + file.originalname);
-  },
-});
-function fileFilter(req, file, cb) {
-  console.log(file);
-  if (file.mimetype.startsWith("text")) {
-    cb(null, true);
-  } else {
-    cb(new AppError("invalid text", 400), false);
-  }
-}
-const upload = multer({ storage, fileFilter })
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "uploads/product");
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     cb(null, uniqueSuffix + "-" + file.originalname);
+//   },
+// });
+// function fileFilter(req, file, cb) {
+//   console.log(file);
+//   if (file.mimetype.startsWith("text")) {
+//     cb(null, true);
+//   } else {
+//     cb(new AppError("invalid text", 400), false);
+//   }
+// }
+// const upload = multer({ storage, fileFilter })
 // end multer
 
 
@@ -32,7 +33,7 @@ const upload = multer({ storage, fileFilter })
 
 productRouter.route('/')
 .get(productController.getAllProduct)
-.post(upload.single('text'),productController.createProduct)
+.post(uploadMixFiles('product',[{name:'imgCover',maxCount:1},{name:'images',maxCount:8}]),productController.createProduct)
 
 
 productRouter.route('/:id')
